12624daeaecf474175cc12782bb0836176e5deac
likhit <likhit@udhaar.co>
2019-08-24 01:50:10 +0530

    [mainFragment]->Main Fragment for showing forecast. [res]->Added resources with drawables and values for colours and dimensions.

--- app/src/main/java/com/uniqolabel/weatherapp/ui/main/MainActivity.java
+++ app/src/main/java/com/uniqolabel/weatherapp/ui/main/MainActivity.java
@@ -4,8 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.uniqolabel.weatherapp.R;
+import com.uniqolabel.weatherapp.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/uniqolabel/weatherapp/ui/main/MainFragment.java
@@ -0,0 +1,18 @@
+package com.uniqolabel.weatherapp.ui.main;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.uniqolabel.weatherapp.R;
+import com.uniqolabel.weatherapp.base.BaseFragment;
+
+public class MainFragment extends BaseFragment {
+
+}
